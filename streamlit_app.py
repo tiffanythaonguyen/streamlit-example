16080b5ad8d36b8291b88a72ad68bf5e4bb18bf6
Tiffany Nguyen <139481517+tiffanythaonguyen@users.noreply.github.com>
2023-07-25 02:32:58 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,10 @@ from io import BytesIO
 
 # For econometrics
 import statsmodels.api as sm
+from statsmodels.sandbox.regression.gmm import IV2SLS
 
 def get_data(data):
+    # Handle data upload types
     if '.csv' in data.name:
         df = pd.read_csv(data)
     elif '.pdf' in data.name:
@@ -23,7 +25,55 @@ def get_data(data):
         df = pd.read_excel(data, engine='openpyxl')
     return df
 
-# ... [rest of the code remains unchanged]
+def show_summary(df, analysis_type):
+    # Show summary based on analysis type
+    if df is not None:
+        st.write(f"## {analysis_type}")
+
+        dependent_var = st.selectbox('Choose Dependent Variable', df.columns)
+        independent_vars = st.multiselect('Choose Independent Variables', df.columns, default=[col for col in df.columns if col != dependent_var])
+
+        X = df[independent_vars]
+        y = df[dependent_var]
+
+        if analysis_type == 'OLS':
+            X = sm.add_constant(X)  # adding a constant
+            model = sm.OLS(y, X).fit()
+            st.write(model.summary())
+        # Continue for other analysis types ...
+
+def main():
+    st.title("Econometric Tools for Business Decisions")
+
+    # Upload data section
+    data_files = st.file_uploader("Upload your company data (CSV, Excel, PDF)", type=['csv', 'xlsx', 'pdf'], accept_multiple_files=True)
+    analysis_type = st.sidebar.selectbox("Choose Analysis Type", ("Home", "OLS", "Multivariable Regression", "R Application", "Machine Learning"))
+
+    if data_files and analysis_type != "Home":
+        col1, col2, col3 = st.beta_columns(3)
+
+        with col1:
+            csv_files = [file for file in data_files if '.csv' in file.name]
+            for file in csv_files:
+                df = get_data(file)
+                st.write(f"### Summary for file: {file.name}")
+                show_summary(df, analysis_type)
+
+        with col2:
+            xlsx_files = [file for file in data_files if '.xlsx' in file.name]
+            for file in xlsx_files:
+                df = get_data(file)
+                st.write(f"### Summary for file: {file.name}")
+                show_summary(df, analysis_type)
+
+        with col3:
+            pdf_files = [file for file in data_files if '.pdf' in file.name]
+            for file in pdf_files:
+                df = get_data(file)
+                st.write(f"### Summary for file: {file.name}")
+                show_summary(df, analysis_type)
+
+    st.markdown("### Check out the full app [here](https://tiffanythaonguyen-streamlit-example-streamlit-app-pia2qx.streamlit.app/)!")
 
 if __name__ == '__main__':
     main()

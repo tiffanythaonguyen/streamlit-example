863add32f9aef33ae1693b6b0c5d988a6759a7d4
Tiffany Nguyen <139481517+tiffanythaonguyen@users.noreply.github.com>
2023-07-25 02:17:39 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,39 +5,58 @@ import pandas as pd
 
 # For econometrics
 import statsmodels.api as sm
+from statsmodels.sandbox.regression.gmm import IV2SLS
+from tabula import read_pdf
+
+# Pages function for each learning objective
+def ols_page(df):
+    st.write("### Ordinary Least Squares (OLS)")
+    # Your OLS code and results here ...
+
+def gls_page(df):
+    st.write("### Generalized Least Squares (GLS)")
+    # Your GLS code and results here ...
+
+def iv_page(df):
+    st.write("### Instrumental-Variables Regression")
+    # Your IV Regression code and results here ...
+
+# Continue for other pages ...
 
 def main():
     st.title("Econometric Tools for Business Decisions")
-    
-    # Upload data section
-    data = st.file_uploader("Upload your company data in CSV format", type=['csv'])
 
+    # Upload data section
+    data = st.file_uploader("Upload your company data (CSV, Excel, PDF)", type=['csv', 'xlsx', 'pdf'])
+    
+    # Handle data upload types
     if data is not None:
-        df = pd.read_csv(data)
+        if '.csv' in data.name:
+            df = pd.read_csv(data)
+        elif '.pdf' in data.name:
+            dfs = read_pdf(data, pages='all', multiple_tables=True)
+            # If there are multiple tables in the PDF, this code simply takes the first one.
+            # You might want to add more logic to handle multiple tables.
+            df = dfs[0]
+        elif '.xlsx' in data.name:
+            df = pd.read_excel(data, engine='openpyxl')
+        
         st.write(df.head())
 
-        analysis_type = st.selectbox(
-            'Select Econometric Analysis',
-            ('OLS', 'GLS', 'Instrumental-Variables Regression', 'Quantile Regression', 'Count Data Models', 'Binary Outcome Models', 'Selection Models')
+        # Multi-page selection
+        page = st.sidebar.selectbox(
+            "Choose Analysis Type",
+            ("Home", "OLS", "GLS", "Instrumental-Variables Regression", "Quantile Regression", "Count Data Models", "Binary Outcome Models", "Selection Models")
         )
 
-        if analysis_type == 'OLS':
-            dependent_var = st.selectbox('Choose Dependent Variable', df.columns)
-            independent_vars = st.multiselect('Choose Independent Variables', df.columns, default=list(df.columns).remove(dependent_var))
+        if page == "OLS":
+            ols_page(df)
+        elif page == "GLS":
+            gls_page(df)
+        elif page == "Instrumental-Variables Regression":
+            iv_page(df)
+        # Continue for other pages ...
 
-            X = df[independent_vars]
-            y = df[dependent_var]
-            
-            X = sm.add_constant(X)  # adding a constant
-
-            model = sm.OLS(y, X).fit()
-            predictions = model.predict(X) 
-
-            st.write(model.summary())
-
-        # Additional sections for other econometric analyses...
-
-    # Link to the provided URL.
     st.markdown("### Check out the full app [here](https://tiffanythaonguyen-streamlit-example-streamlit-app-pia2qx.streamlit.app/)!")
 
 if __name__ == '__main__':

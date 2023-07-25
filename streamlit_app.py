5a58cef0e2e120f77442f6ef19cf4b00c4f0957a
Tiffany Nguyen <139481517+tiffanythaonguyen@users.noreply.github.com>
2023-07-25 02:27:53 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,62 +2,28 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import PyPDF2
+from io import BytesIO
 
 # For econometrics
 import statsmodels.api as sm
-from statsmodels.sandbox.regression.gmm import IV2SLS
-from tabula import read_pdf
 
-# Pages function for each learning objective
-def ols_page(df):
-    st.write("### Ordinary Least Squares (OLS)")
-    # Your OLS code and results here ...
+def get_data(data):
+    if '.csv' in data.name:
+        df = pd.read_csv(data)
+    elif '.pdf' in data.name:
+        # Extract text using PyPDF2
+        pdf_reader = PyPDF2.PdfFileReader(data)
+        text = ""
+        for page_num in range(pdf_reader.numPages):
+            text += pdf_reader.getPage(page_num).extractText()
+        st.text_area("Extracted PDF Text", text, height=300)  # Displaying extracted text for now
+        return None  # Adjust this as needed
+    elif '.xlsx' in data.name:
+        df = pd.read_excel(data, engine='openpyxl')
+    return df
 
-def gls_page(df):
-    st.write("### Generalized Least Squares (GLS)")
-    # Your GLS code and results here ...
-
-def iv_page(df):
-    st.write("### Instrumental-Variables Regression")
-    # Your IV Regression code and results here ...
-
-# Continue for other pages ...
-
-def main():
-    st.title("Econometric Tools for Business Decisions")
-
-    # Upload data section
-    data = st.file_uploader("Upload your company data (CSV, Excel, PDF)", type=['csv', 'xlsx', 'pdf'])
-    
-    # Handle data upload types
-    if data is not None:
-        if '.csv' in data.name:
-            df = pd.read_csv(data)
-        elif '.pdf' in data.name:
-            dfs = read_pdf(data, pages='all', multiple_tables=True)
-            # If there are multiple tables in the PDF, this code simply takes the first one.
-            # You might want to add more logic to handle multiple tables.
-            df = dfs[0]
-        elif '.xlsx' in data.name:
-            df = pd.read_excel(data, engine='openpyxl')
-        
-        st.write(df.head())
-
-        # Multi-page selection
-        page = st.sidebar.selectbox(
-            "Choose Analysis Type",
-            ("Home", "OLS", "GLS", "Instrumental-Variables Regression", "Quantile Regression", "Count Data Models", "Binary Outcome Models", "Selection Models")
-        )
-
-        if page == "OLS":
-            ols_page(df)
-        elif page == "GLS":
-            gls_page(df)
-        elif page == "Instrumental-Variables Regression":
-            iv_page(df)
-        # Continue for other pages ...
-
-    st.markdown("### Check out the full app [here](https://tiffanythaonguyen-streamlit-example-streamlit-app-pia2qx.streamlit.app/)!")
+# ... [rest of the code remains unchanged]
 
 if __name__ == '__main__':
     main()

6e87d8066eca9674295aa55804ecff9a9610bf68
Tiffany Nguyen <139481517+tiffanythaonguyen@users.noreply.github.com>
2023-07-25 04:35:26 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,29 +2,26 @@ import streamlit as st
 import pandas as pd
 import pdfreader
 from io import BytesIO
-import pandera as pa
-from scipy import stats
 import matplotlib.pyplot as plt
 import seaborn as sns
+from matplotlib import ticker  # Necessary for MaxNLocator
 
-st.set_page_config(
-    page_title="FinanceEconTool",
-    page_icon="📈",
-    layout="wide",
-    initial_sidebar_state="collapsed"
-)
+def extract_content_from_file(file):
+    """
+    Extracts content from various file types and returns them.
+    """
+    if '.csv' in file.name:
+        df = pd.read_csv(file)
+        return df
 
-# Define schema for tidy dataset validation
-schema = pa.DataFrameSchema({
-    "Feedback": pa.Column(pa.String),
-    "Hypothesis Test Question": pa.Column(pa.String),
-    "Hypothesis Test Result": pa.Column(pa.String),
-})
+    elif '.pdf' in file.name:
+        reader = pdfreader.SimplePDFViewer(file)
+        reader.navigate(1)
+        reader.render()
+        text = " ".join(reader.canvas.strings)
+        return text
 
-def perform_hypothesis_test(question, text):
-    word_count = len(text.split())
-    p_value = stats.binom_test(word_count, n=500, p=0.5)  # Hypothetical binomial test
-    return "Accept" if p_value > 0.05 else "Reject"
+    return None
 
 def main():
     st.title("FinanceEconTool 💼📈🔬")
@@ -36,6 +33,9 @@ def main():
         ["Math for Finance and Analytics with R", "Analytics for Finance", "Database Management Systems - SQL", 
          "Data Science with Python", "Econometrics"]
     )
+
+    # Sidebar header for Coursework
+    st.sidebar.header("Coursework")
 
     # File Uploader
     uploaded_files = st.file_uploader("Upload Files", type=['csv', 'pdf'], accept_multiple_files=True)
@@ -52,31 +52,48 @@ def main():
                 plot_options = ["Bar plot", "Scatter plot", "Histogram", "Box plot"]
                 selected_plot = st.sidebar.selectbox("Choose a plot type", plot_options)
 
-                # Visualization selection
-                # The given code for visualization is incorporated here.
-                # ... [Your visualization code]
+                if selected_plot == "Bar plot":
+                    x_axis = st.sidebar.selectbox("Select x-axis", data.columns)
+                    y_axis = st.sidebar.selectbox("Select y-axis", data.columns)
+                    st.write("Bar plot:")
+                    fig, ax = plt.subplots()
+                    sns.barplot(x=data[x_axis], y=data[y_axis], ax=ax)
+
+                    # Modify the x-axis ticks
+                    ax.xaxis.set_major_locator(ticker.MaxNLocator(integer=True, nbins=10))
+                    ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha="right")
+                    
+                    st.pyplot(fig)
+
+                # ... Add other plots here ...
 
             # If PDF file
             elif '.pdf' in file.name:
-                reader = pdfreader.SimplePDFViewer(file)
-                reader.navigate(1)
-                reader.render()
-                text = " ".join(reader.canvas.strings)
-                st.text_area("PDF Content", text, height=300)
-
-                # Hypothesis Testing
-                question = st.text_input("Ask a question about the document for hypothesis testing")
-                if question:
-                    result = perform_hypothesis_test(question, text)
-                    st.write(f"Hypothesis Test Result: {result}")
+                content = extract_content_from_file(file)
+                st.text_area("PDF Content", content, height=300)
 
             else:
                 st.write("Unsupported file format or empty content.")
 
             # If you want to see basic statistics from CSV files
-            if isinstance(data, pd.DataFrame):
+            if isinstance(content, pd.DataFrame):
                 st.subheader(f"Data Statistics for {file.name}")
-                st.write(data.describe())
+                st.write(f"Total Number of Rows: {len(content)}")
+                st.write(f"Number of Columns: {len(content.columns)}")
+                st.write(f"Columns: {', '.join(content.columns)}")
+                st.write(f"Data Types:\n{content.dtypes}")
+                st.write(f"Summary Statistics:\n{content.describe()}")
+
+            # Summary Dashboard for Insights
+            st.subheader("Summary Dashboard for Insights")
+            if isinstance(content, pd.DataFrame):
+                numeric_columns = content.select_dtypes(include='number').columns
+                st.write(f"Summary Statistics for Numeric Columns:")
+                st.write(content[numeric_columns].describe())
+                st.write(f"Correlation Matrix for Numeric Columns:")
+                st.write(content[numeric_columns].corr())
+
+                st.write(f"Top 10 Key Technical Words:")
 
 if __name__ == '__main__':
     main()

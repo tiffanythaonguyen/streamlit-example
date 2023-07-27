789ee2234869663cdf5c1b0e6ad381190050d9c3
PyFiTech <139481517+tiffanythaonguyen@users.noreply.github.com>
2023-07-26 19:50:33 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import statsmodels.api as sm
 import seaborn as sns
 import matplotlib.pyplot as plt
 
@@ -22,26 +21,19 @@ def analyze_data(df):
     st.write(f"Total Columns: {df.shape[1]}")
     st.write(f"Total Rows: {df.shape[0]}")
     st.write(f"Index: {df.index}")
-    
+
     for column in df.columns:
         st.write(f"Unique values in {column}: {df[column].nunique()}")
-    
-    # Regression analysis
-    if 'prcc_f' in df.columns and all(col in df.columns for col in ['sale', 'cogs', 'ppegt']):
-        X = df[['sale', 'cogs', 'ppegt']].dropna()
-        y = df.loc[X.index, 'prcc_f']
-        X = sm.add_constant(X)  # adding a constant
-        model = sm.OLS(y, X).fit()
-        st.write(model.summary())
 
-        # Correlation heatmap
-        st.subheader('Correlation Matrix')
-        corr = df[['prcc_f', 'sale', 'cogs', 'ppegt']].corr()
-        fig, ax = plt.subplots(figsize=(10, 6))
-        sns.heatmap(corr, annot=True, ax=ax, cmap="coolwarm", linewidths=.5, fmt=".2f")
-        st.pyplot(fig)
-    else:
-        st.warning("The necessary columns for regression analysis are not found in the uploaded CSV.")
+    # Plot total of numeric columns in a bar graph
+    sum_data = df.sum(numeric_only=True)
+    fig, ax = plt.subplots(figsize=(12, 6))
+    sum_data.plot(kind='bar', ax=ax)
+    ax.set_title('Sum of Numeric Columns')
+    ax.set_ylabel('Total')
+    ax.set_xlabel('Columns')
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha="right")
+    st.pyplot(fig)
 
     # Short description of the data
     st.subheader("Short Description")
@@ -49,14 +41,12 @@ def analyze_data(df):
 
 def main():
     st.title("PyFiQuant 💼📈🔬")
-    st.write("""
-    **Principles of this Exercise**:
-    - **Data Transparency**: Openness about data sources, methodologies, and assumptions.
-    - **Digitalization**: Using modern methods and tools for data processing and presentation.
-    - **Accountability**: Taking responsibility for data accuracy and interpretation.
-    - **Inclusiveness**: Ensuring the tool is accessible and usable by a wide audience.
-    """)
     
+    st.sidebar.markdown("""
+    **PyFiQuant** is designed to enhance data transparency, digitalization, 
+    accountability, and inclusiveness in the realm of finance and economics.
+    """)
+
     uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
     if uploaded_file is not None:
         df = pd.read_csv(uploaded_file)
@@ -64,26 +54,6 @@ def main():
         
         if st.button("Analyze Data"):
             analyze_data(df)
-            
-        # Visualization section
-        st.sidebar.header("Visualizations")
-        selected_plot = st.sidebar.radio("Select plot type", ["Bar plot", "Histogram"])
-        
-        if selected_plot == "Bar plot":
-            x_axis = st.sidebar.selectbox("Select x-axis", df.columns)
-            y_axis = st.sidebar.selectbox("Select y-axis", df.columns)
-            st.write("Bar plot:")
-            fig, ax = plt.subplots(figsize=(10, 6))
-            sns.barplot(x=df[x_axis], y=df[y_axis], ax=ax)
-            ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha="right")
-            st.pyplot(fig)
-        
-        elif selected_plot == "Histogram":
-            selected_col = st.sidebar.selectbox("Select column for histogram", df.columns)
-            st.write("Histogram:")
-            fig, ax = plt.subplots(figsize=(10, 6))
-            sns.histplot(df[selected_col], ax=ax, bins=30, kde=True)
-            st.pyplot(fig)
 
 if __name__ == "__main__":
     main()

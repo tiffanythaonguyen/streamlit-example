6ecfe4fd9ef9c05ca4274f8fea930d0a8c6d73f6
Tiffany Nguyen <139481517+tiffanythaonguyen@users.noreply.github.com>
2023-09-01 05:20:06 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,38 @@
 import streamlit as st
 import re
-from pandas import DataFrame
-from keybert import KeyBERT
-import seaborn as sns
+from collections import Counter
+import nltk
+from nltk.corpus import stopwords
+from nltk.collocations import BigramCollocationFinder
+from nltk.metrics import BigramAssocMeasures
+
+# Download stopwords if not already downloaded
+nltk.download('stopwords')
 
 # Function to identify numerical values in the text
 def identify_numerical_values(doc):
     dollar_values = re.findall(r'\$\d+(\.\d{2})?|\$\d{1,3}(?:,\d{3})*(?:\.\d{2})?', doc)
     percent_values = re.findall(r'\d+(\.\d+)?%', doc)
     by_year_values = re.findall(r'by \d{4}', doc)
-    general_numerical_values = re.findall(r'\d+(\.\d+)?|\d{1,3}(?:,\d{3})*(?:\.\d+)?', doc)
-    return dollar_values, percent_values, by_year_values, general_numerical_values
+    return dollar_values, percent_values, by_year_values
 
-# Function to identify abbreviations and capitalized words
-def identify_abbreviations_and_caps(doc):
-    abbreviations = re.findall(r'\b[A-Z]{2,}\b', doc)
-    capitalized_words = re.findall(r'\b[A-Z][a-z]+\b', doc)
-    return abbreviations, capitalized_words
+# Function to identify keywords by frequency
+def identify_keywords_by_frequency(doc):
+    words = re.findall(r'\b\w+\b', doc.lower())
+    filtered_words = [word for word in words if word not in stopwords.words('english')]
+    return Counter(filtered_words).most_common(10)
+
+# Function to identify relationships between words
+def identify_word_relationships(doc):
+    words = re.findall(r'\b\w+\b', doc.lower())
+    filtered_words = [word for word in words if word not in stopwords.words('english')]
+    bigram_finder = BigramCollocationFinder.from_words(filtered_words)
+    bigrams = bigram_finder.nbest(BigramAssocMeasures.likelihood_ratio, 10)
+    return bigrams
 
 # Main function
 def main():
-    st.title("Keyword Extractor 🎈")
+    st.title("BERT Keyword Extractor 🎈")
     
     st.markdown("## 📌 Paste Document")
     
@@ -31,31 +43,20 @@ def main():
     if st.button("Analyze Text"):
         
         # Identify numerical values
-        dollar_values, percent_values, by_year_values, general_numerical_values = identify_numerical_values(doc)
+        dollar_values, percent_values, by_year_values = identify_numerical_values(doc)
         
         # Display identified values
         st.write(f"Identified dollar values: {dollar_values}")
         st.write(f"Identified percent values: {percent_values}")
         st.write(f"Identified 'by year' values: {by_year_values}")
-        st.write(f"Identified general numerical values: {general_numerical_values}")
 
-        # Identify abbreviations and capitalized words
-        abbreviations, capitalized_words = identify_abbreviations_and_caps(doc)
-        
-        # Display identified abbreviations and capitalized words
-        st.write(f"Identified abbreviations: {abbreviations}")
-        st.write(f"Identified capitalized words: {capitalized_words}")
+        # Identify keywords by frequency
+        keywords_by_frequency = identify_keywords_by_frequency(doc)
+        st.write(f"Identified keywords by frequency: {keywords_by_frequency}")
 
-        # Initialize KeyBERT model
-        kw_model = KeyBERT("distilbert-base-nli-mean-tokens")
-        
-        # Extract keywords
-        keywords = kw_model.extract_keywords(doc, keyphrase_ngram_range=(1, 2), stop_words=None, top_n=10, diversity=0.5)
-        
-        # Display keywords
-        df = DataFrame(keywords, columns=["Keyword/Keyphrase", "Relevancy"]).sort_values(by="Relevancy", ascending=False).reset_index(drop=True)
-        df.index += 1
-        st.table(df)
+        # Identify relationships between words
+        word_relationships = identify_word_relationships(doc)
+        st.write(f"Identified relationships between words: {word_relationships}")
 
 if __name__ == "__main__":
     main()

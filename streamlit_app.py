e24f0f9c6ca9a82bcfad71d7a8bc4c60862d517a
Tiffany Nguyen <139481517+tiffanythaonguyen@users.noreply.github.com>
2023-09-01 07:44:59 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,5 @@
 import streamlit as st
 import re  # Importing the regular expression library
-import fitz  # PyMuPDF
-from wordcloud import WordCloud
-import requests
-import matplotlib.pyplot as plt
-from sklearn.feature_extraction.text import TfidfVectorizer
 
 # Function to identify numerical values and the next two words or any key after each value
 def identify_numerical_values(doc):
@@ -34,65 +29,6 @@ def main():
         st.write(f"Identified decimal numbers: {decimal_numbers}")
         st.write(f"Identified months: {months}")
         st.write(f"Identified years: {years}")
-
-# Function to add object detection dataframe table
-def object_detection_page():
-    st.title("Object Detection Dataframe Table")
-    # Create a sample dataframe
-    import pandas as pd
-    data = {
-        "Layout": [1, 2, 3],
-        "Index": [101, 102, 103],
-        "Image": ["img1.jpg", "img2.jpg", "img3.jpg"],
-        "Background": ["Blue", "Green", "Red"],
-        "Objectives": ["Obj1", "Obj2", "Obj3"],
-        "Goals": ["Goal1", "Goal2", "Goal3"],
-        "Timestamp": ["2023-09-01", "2023-09-02", "2023-09-03"],
-        "URL": ["url1", "url2", "url3"]
-    }
-    df = pd.DataFrame(data)
-    st.dataframe(df)
-
-# Function to generate Word Cloud
-def generate_word_cloud():
-    pdf_urls = [
-        'https://ocw.mit.edu/courses/15-031j-energy-decisions-markets-and-policies-spring-2012/ba02e762046b99e81578e7d19980bb22_MIT15_031JS12_lec10.pdf',
-        'https://ocw.mit.edu/courses/15-433-investments-spring-2003/c5845cd981c2e63f7ff303c92c7d41be_154332random_walk.pdf'
-    ]
-    
-    combined_text = ''
-    
-    for pdf_url in pdf_urls:
-        response = requests.get(pdf_url)
-        pdf_content = response.content
-        pdf_document = fitz.open(stream=pdf_content, filetype="pdf")
-        num_pages = pdf_document.page_count
-        for page_num in range(num_pages):
-            page = pdf_document.load_page(page_num)
-            combined_text += page.get_text("text")
-        pdf_document.close()
-
-    vectorizer = TfidfVectorizer()
-    tfidf_matrix = vectorizer.fit_transform([combined_text])
-
-    # Create a WordCloud object
-    wordcloud = WordCloud(width=800, height=400, background_color='white').generate(combined_text)
-
-    plt.figure(figsize=(10, 5))
-    plt.imshow(wordcloud, interpolation='bilinear')
-    plt.title("Combined Word Cloud for Open Source PDFs")
-    plt.axis('off')
-    plt.show()
-
-# App navigation
-app_pages = {
-    "📊 Business Analysis": main,
-    "🔍 Object Detection": object_detection_page,
-    "🌟 Word Cloud": generate_word_cloud
-}
-
-selected_page = st.sidebar.selectbox("Select a page", list(app_pages.keys()))
-app_pages[selected_page]()
 
 if __name__ == "__main__":
     main()

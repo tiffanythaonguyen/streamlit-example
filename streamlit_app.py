e645864d02383006ed02adc542e22c7d670b42c3
Tiffany Nguyen <139481517+tiffanythaonguyen@users.noreply.github.com>
2023-09-01 06:07:04 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,19 @@
 import streamlit as st
 import re  # Importing the regular expression library
 
-# Function to identify numerical values and the next two words after each value
+# Function to add vertical space
+def add_vertical_space(num_lines: int = 1):
+    """Add vertical space to your Streamlit app."""
+    for _ in range(num_lines):
+        st.write("")
+
+# Function to identify numerical values and the next two words or any key after each value
 def identify_numerical_values(doc):
-    # Identify $100,000 format
-    dollar_values = re.findall(r'(\$\d{1,3}(?:,\d{3})*)(?:\s+(\w+\s+\w+))?', doc)
-    # Identify % format
-    percent_values = re.findall(r'(\d{1,3}%)(?:\s+(\w+\s+\w+))?', doc)
-    # Identify whole numbers with commas
-    whole_numbers = re.findall(r'(\b\d{1,3}(?:,\d{3})*\b)(?:\s+(\w+\s+\w+))?', doc)
-    # Identify decimal numbers in x.x or x.xx format
-    decimal_numbers = re.findall(r'(\b\d+\.\d{1,2}\b)(?:\s+(\w+\s+\w+))?', doc)
-    # Identify months
+    dollar_values = re.findall(r'(\$\d{1,3}(?:,\d{3})*)(?:\s+([\w\s\W]{1,20}))?', doc)
+    percent_values = re.findall(r'(\d{1,3}%)(?:\s+([\w\s\W]{1,20}))?', doc)
+    whole_numbers = re.findall(r'(\b\d{1,3}(?:,\d{3})*\b)(?!%)(?:\s+([\w\s\W]{1,20}))?', doc)
+    decimal_numbers = re.findall(r'(\b\d+\.\d{1,2}\b)(?:\s+([\w\s\W]{1,20}))?', doc)
     months = re.findall(r'\b(January|February|March|April|May|June|July|August|September|October|November|December)\b', doc)
-    # Identify years in xxxx format
     years = re.findall(r'\b\d{4}\b', doc)
     
     return dollar_values, percent_values, whole_numbers, decimal_numbers, months, years
@@ -24,16 +24,11 @@ def main():
     
     st.markdown("## 📌 Paste Document")
     
-    # Text area for document input
     doc = st.text_area("Paste your text below (max 500 words)", height=200)
     
-    # Button to trigger analysis
     if st.button("Analyze Text"):
-        
-        # Identify numerical values and the next two words after each value
         dollar_values, percent_values, whole_numbers, decimal_numbers, months, years = identify_numerical_values(doc)
         
-        # Display identified values
         st.write(f"Identified dollar values: {dollar_values}")
         st.write(f"Identified percent values: {percent_values}")
         st.write(f"Identified whole numbers: {whole_numbers}")
@@ -41,7 +36,10 @@ def main():
         st.write(f"Identified months: {months}")
         st.write(f"Identified years: {years}")
 
-        # Your existing code for keyword extraction can go here
+    # Add vertical space
+    add_n_lines = st.slider("Add n vertical lines below this", 1, 20, 5)
+    add_vertical_space(add_n_lines)
+    st.write("Here is text after the nth line!")
 
 if __name__ == "__main__":
     main()

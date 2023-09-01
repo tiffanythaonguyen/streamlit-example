bb2de952abbbf005ed1b975dbe4d6817911dcd17
Tiffany Nguyen <139481517+tiffanythaonguyen@users.noreply.github.com>
2023-09-01 05:59:06 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,22 @@
 import streamlit as st
 import re  # Importing the regular expression library
 
-# Function to identify numerical values and the next word after each value
+# Function to identify numerical values and the next two words after each value
 def identify_numerical_values(doc):
     # Identify $100,000 format
-    dollar_values = re.findall(r'(\$\d{1,3}(?:,\d{3})*)(?:\s+(\w+))?', doc)
+    dollar_values = re.findall(r'(\$\d{1,3}(?:,\d{3})*)(?:\s+(\w+\s+\w+))?', doc)
     # Identify % format
-    percent_values = re.findall(r'(\d{1,3}%)(?:\s+(\w+))?', doc)
-    # Identify whole and decimal numbers
-    numbers = re.findall(r'(\b\d+(\.\d+)?\b)(?:\s+(\w+))?', doc)
+    percent_values = re.findall(r'(\d{1,3}%)(?:\s+(\w+\s+\w+))?', doc)
+    # Identify whole numbers with commas
+    whole_numbers = re.findall(r'(\b\d{1,3}(?:,\d{3})*\b)(?:\s+(\w+\s+\w+))?', doc)
+    # Identify decimal numbers in x.x or x.xx format
+    decimal_numbers = re.findall(r'(\b\d+\.\d{1,2}\b)(?:\s+(\w+\s+\w+))?', doc)
     # Identify months
     months = re.findall(r'\b(January|February|March|April|May|June|July|August|September|October|November|December)\b', doc)
     # Identify years in xxxx format
     years = re.findall(r'\b\d{4}\b', doc)
     
-    return dollar_values, percent_values, numbers, months, years
+    return dollar_values, percent_values, whole_numbers, decimal_numbers, months, years
 
 # Main function
 def main():
@@ -28,13 +30,14 @@ def main():
     # Button to trigger analysis
     if st.button("Analyze Text"):
         
-        # Identify numerical values and the next word after each value
-        dollar_values, percent_values, numbers, months, years = identify_numerical_values(doc)
+        # Identify numerical values and the next two words after each value
+        dollar_values, percent_values, whole_numbers, decimal_numbers, months, years = identify_numerical_values(doc)
         
         # Display identified values
         st.write(f"Identified dollar values: {dollar_values}")
         st.write(f"Identified percent values: {percent_values}")
-        st.write(f"Identified whole and decimal numbers: {numbers}")
+        st.write(f"Identified whole numbers: {whole_numbers}")
+        st.write(f"Identified decimal numbers: {decimal_numbers}")
         st.write(f"Identified months: {months}")
         st.write(f"Identified years: {years}")
 

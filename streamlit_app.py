91d5f1f2722a9d8a531d16573e5429f724ec5b84
Tiffany Nguyen <139481517+tiffanythaonguyen@users.noreply.github.com>
2023-09-01 05:43:01 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,20 @@
 import streamlit as st
 import re  # Importing the regular expression library
 
-# Function to identify numerical values in the text
+# Function to identify numerical values, months, and years in the text
 def identify_numerical_values(doc):
     # Identify $100,000 format
     dollar_values = re.findall(r'\$\d{1,3}(?:,\d{3})*', doc)
     # Identify % format
     percent_values = re.findall(r'\d{1,3}%', doc)
-    # Identify "by 'year'" format
-    by_year_values = re.findall(r'by \d{4}', doc)
+    # Identify general numerical values
+    general_numerical_values = re.findall(r'\b\d+\b', doc)
+    # Identify months
+    months = re.findall(r'\b(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)\b', doc, re.IGNORECASE)
+    # Identify years
+    years = re.findall(r'\b\d{4}\b', doc)
     
-    return dollar_values, percent_values, by_year_values
+    return dollar_values, percent_values, general_numerical_values, months, years
 
 # Main function
 def main():
@@ -24,13 +28,15 @@ def main():
     # Button to trigger analysis
     if st.button("Analyze Text"):
         
-        # Identify numerical values
-        dollar_values, percent_values, by_year_values = identify_numerical_values(doc)
+        # Identify numerical values, months, and years
+        dollar_values, percent_values, general_numerical_values, months, years = identify_numerical_values(doc)
         
         # Display identified values
         st.write(f"Identified dollar values: {dollar_values}")
         st.write(f"Identified percent values: {percent_values}")
-        st.write(f"Identified 'by year' values: {by_year_values}")
+        st.write(f"Identified general numerical values: {general_numerical_values}")
+        st.write(f"Identified months: {months}")
+        st.write(f"Identified years: {years}")
 
         # Your existing code for keyword extraction can go here
 

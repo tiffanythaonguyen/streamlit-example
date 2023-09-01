45451978a8fc138021db0f532d354507b7d9fe98
Tiffany Nguyen <139481517+tiffanythaonguyen@users.noreply.github.com>
2023-09-01 04:32:35 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,49 +1,75 @@
 import streamlit as st
 import pandas as pd
 import statsmodels.api as sm
+import networkx as nx
+import matplotlib.pyplot as plt
+from streamlit_extras.add_vertical_space import add_vertical_space  # Import the add_vertical_space function
+
+# Function for colored headers
+def colored_header(label: str = "Nice title", description: str = "Cool description", color_name: str = "red"):
+    st.subheader(label)
+    st.write(
+        f'<hr style="background-color: {color_name}; margin-top: 0;'
+        ' margin-bottom: 0; height: 3px; border: none; border-radius: 3px;">',
+        unsafe_allow_html=True,
+    )
+    if description:
+        st.caption(description)
 
 # Function for basic statistical analysis
 def analyze_data(df):
-    # Basic insights
-    st.write(f"Total Columns: {df.shape[1]}")
-    st.write(f"Total Rows: {df.shape[0]}")
-    st.write(f"Index: {df.index}")
-    for column in df.columns:
-        st.write(f"Unique values in {column}: {df[column].nunique()}")
-    
-    # Regression (for demonstration, we'll assume 'prcc_f' as dependent variable)
-    # Using 'sale', 'cogs', and 'ppegt' as independent variables
-    X = df[['sale', 'cogs', 'ppegt']].dropna()
-    y = df.loc[X.index, 'prcc_f']
-    X = sm.add_constant(X)  # adding a constant
-    model = sm.OLS(y, X).fit()
-    st.write(model.summary())
+    # Your existing code for statistical analysis here
 
-    # Correlation
-    st.subheader('Correlation Matrix')
-    st.write(df[['prcc_f', 'sale', 'cogs', 'ppegt']].corr())
-
-    # Short description of the data
-    st.subheader("Short Description")
-    st.write("""
-    This dataset provides financial indicators for various companies across different years, 
-    ranging from asset details to income metrics. The dataset includes information such as 
-    sales, cost of goods sold, property, plant, and equipment (gross), and the closing stock 
-    price, among other variables.
-    """)
+# Function to visualize the black box decision-making process
+def visualize_black_box():
+    # Your existing code for visualizing the black box here
 
 # Main function
 def main():
-    st.title("PyFiQuant 💼📈🔬")
+    colored_header("Person-Oriented AI: Data Hosting and Sharing Platform", "Improving public perceptions on open-source technology solutions", "blue")
     
+    # Sidebar for Page Selection
+    st.sidebar.header("Pages")
+    page_choice = st.sidebar.selectbox("Choose a Page", ["Business/Strategy", "Finance/Investing", "People/Environment"])
+
+    # Business/Strategy Page
+    if page_choice == "Business/Strategy":
+        colored_header("Business/Strategy", "Tools and insights related to business strategy", "green")
+        # Your code for Business/Strategy functionalities here
+
+    # Finance/Investing Page
+    elif page_choice == "Finance/Investing":
+        colored_header("Finance/Investing", "Tools and insights related to finance and investing", "orange")
+        # Your code for Finance/Investing functionalities here
+
+    # People/Environment Page
+    elif page_choice == "People/Environment":
+        colored_header("People/Environment", "Tools and insights related to people and the environment", "purple")
+        # Your code for People/Environment functionalities here
+
+    # Black Box Visualization
+    visualize_black_box()
+
+    # Outputs
+    st.subheader("Outputs")
+    actual_outcome = st.text_input("Actual Outcome")
+
+    # Save to Database (Placeholder)
+    if st.button("Save to Database"):
+        st.success("Saved to database.")
+
+    # Add vertical space
+    add_n_lines = st.slider("Add n vertical lines below this", 1, 20, 5)
+    add_vertical_space(add_n_lines)
+    st.write("Here is text after the nth line!")
+
+    # Existing code for data analysis
     uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
     if uploaded_file is not None:
         df = pd.read_csv(uploaded_file)
         st.write(df.head())
-        
         if st.button("Analyze Data"):
             analyze_data(df)
 
 if __name__ == "__main__":
     main()
-

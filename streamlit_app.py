e25d565361a0eb2562d54934e68d52cafaa4b448
Tiffany Nguyen <139481517+tiffanythaonguyen@users.noreply.github.com>
2023-09-06 15:48:04 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,28 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import mitosheet  # Import mitosheet library
 
 def main():
-    st.title("Mito Spreadsheet Automation for Financial Data")
+    st.title("Financial Data Spreadsheet Automation")
 
     # Initialize an empty DataFrame
     empty_df = pd.DataFrame()
 
     # Display empty DataFrame as a table in Streamlit
+    st.write("Welcome to the Financial Data Spreadsheet Automation App!")
+    st.write("This app is designed to bridge the gap between profound human questions and quantitative research in the financial domain.")
+    st.write("From philosophers pondering the nature of value to data engineers optimizing algorithms, from CFOs making strategic decisions to investors managing portfolios, this app provides a continuum of tools.")
+    st.write("It empowers users to explore, analyze, and derive insights from financial data.")
+    st.write("Use it to bring quantitative research to life, whether you're an academic researcher or a CEO shaping investment strategies.")
+
     st.write("Empty Spreadsheet:")
-    st.write(empty_df)
+    
+    # Create an empty spreadsheet using mitosheet
+    empty_df = mitosheet.sheet(data=empty_df, key="empty_sheet")
+    
+    # Display the empty spreadsheet
+    st.dataframe(empty_df)
 
     # Upload CSV or Excel file
     uploaded_file = st.file_uploader("Choose a CSV or Excel file", type=["csv", "xlsx"])
@@ -24,7 +36,10 @@ def main():
                 df = pd.read_excel(uploaded_file, engine='openpyxl')
 
             st.write("Uploaded Spreadsheet:")
-            st.write(df)
+            
+            # Display the uploaded data using mitosheet
+            df = mitosheet.sheet(data=df, key="uploaded_sheet")
+            st.dataframe(df)
 
             # Perform basic statistical summaries using NumPy
             if st.button("Show Basic Statistics"):

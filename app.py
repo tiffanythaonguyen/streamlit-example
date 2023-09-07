d685a9dc875b0ad0cb4419b62f65489506cbf920
Tiffany Nguyen <139481517+tiffanythaonguyen@users.noreply.github.com>
2023-09-06 17:35:03 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
-[![Streamlit App](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://tiffanythaonguyen-streamlit-example-streamlit-app-pia2qx.streamlit.app/)
-
-
+git add app.py  # Replace 'app.py' with the correct filename if it's different.
+git commit -m "Add app.py"  # Add a meaningful commit message.
+git push origin main  # Push the changes to your GitHub repository's main branch.
